9c256f76064e49919e7b2d60da5045a854c1051c
gperez13 <giovanni_perez4@yahoo.com>
2018-03-05 18:05:40 -0600

    Created Items model, migration, and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+
+50.times do
+	
+	Item.create({
+		name: Faker::Hipster.sentence(3),
+		description: Faker::Hipster.paragraph,
+		price: Faker::Number.decimal(2, 2).to_f
+		})	
+end
