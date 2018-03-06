5431d4b66c6fa88f31024513042f634633a4d3e6
gperez13 <giovanni_perez4@yahoo.com>
2018-03-05 18:57:44 -0600

    Finished RESTful api. Authentication, user cart, usage and additional tables/models still need to be made

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -26,6 +26,23 @@ module Api
 				end
 			end
 
+			def destroy
+				item = Item.find(params[:id])
+				item.destroy
+				render json: {status: 'SUCCESS', msg: 'Deleted item', data:item}, status: :ok
+			end
+
+			def update
+				item = Item.find(params[:id])
+				if item.update_attributes(item_params)
+				render json: {status: 'SUCCESS', msg: 'Updated item', data:item}, status: :ok
+
+				else
+					render json: {status: 'ERROR', msg: 'Item Not Updated', 
+					data:item.errors}, status: :unprocessable_entity
+				end
+			end
+
 			private
 
 			def item_params
