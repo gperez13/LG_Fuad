fc85c8f08ea26c4c89bc90c97cf399ee83967287
gperez13 <giovanni_perez4@yahoo.com>
2018-03-05 18:40:48 -0600

    Included show route. Working on create

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,10 +1,43 @@
 module Api
 	module V1
 		class ItemsController < ApplicationController
+
+
 			def index
 				items = Item.order('created_at DESC');
 				render json: {status: 'SUCCESS', msg: 'Loaded items', data:items}, status: :ok
 			end
+
+			def show
+				item = Item.find(params[:id])
+				render json: {status: 'SUCCESS', msg: 'Loaded item', data:item}, status: :ok
+
+			end
+
+			def create
+				item = Item.new(item_params)
+
+				if article.save
+					render json: {status: 'SUCCESS', msg: 'Saved item', data:item}, status: :ok
+				
+				else
+					render json: {status: 'ERROR', msg: 'Item Not Saved', 
+					data:item.errors}, status: :unprocessable_entity
+
+				end
+
+			end
+
+			private
+
+			def article_params
+				params.permit(:name, :description, :price)
+			end
+
+
+
+
+
 		end
 	end
 end
